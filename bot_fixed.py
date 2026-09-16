5cc5283ee818f5eeacac6a84133cb38c5b4ba182
abcdef74589-max <abcdef74589@gmail.com>
2026-09-16 11:55:49 +0330

    Update bot_fixed.py

--- bot_fixed.py
+++ bot_fixed.py
@@ -753,10 +753,10 @@ async def main():
                     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         asyncio.run(main())
-
-    except Exception:
-        logging.exception(
-            "FATAL
+    except Exception as e:
+        logging.exception('FATAL ERROR: %s', e)
+        print(f'ERROR: {type(e).__name__}: {e}', flush=True)
+        
